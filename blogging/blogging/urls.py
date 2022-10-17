5e5029f47fc9bc5ff837efb9b9ee16f913413ac4
Rohit Raj <rohit06092000@gmail.com>
2022-10-18 02:39:01 +0530

    Updated

--- blogging/blogging/urls.py
+++ blogging/blogging/urls.py
@@ -15,9 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from django.urls import include
+from Users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("bllog.urls"))
+    path('register/',user_views.register,name = "register"),
+    path('profile/',user_views.Profile,name = "profile"),
+    path('login/',auth_views.LoginView.as_view(template_name = 'Users/login.html'),name = "login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'Users/logout.html'),name = "logout"),
+    path('', include("bllog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
